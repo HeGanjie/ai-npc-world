903091158230cf598335de60fa5a9e89c5b65d23
heganjie <heganjie@gmail.com>
2023-04-30 18:36:20 +0800

    draw background map

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,81 @@
-import {BlurFilter, TextStyle} from 'pixi.js';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
-import { useMemo } from 'react';
+import {useEffect, useRef, useState} from 'react';
+import {Sprite, Stage} from '@pixi/react';
+import * as PIXI from 'pixi.js';
+import timeMapUrl from './assets/pixelholes-overworld-tileset-demo.png' // https://pixelhole.itch.io/pixelholes-overworld-tileset
 
-export const MyComponent = () =>
-{
-    const blurFilter = useMemo(() => new BlurFilter(0.5), []);
+const calculateScale = () => {
+    const imageWidth = 1872 // 图片宽度;
+    const imageHeight = 1248 // 图片高度;
+
+    const scaleX = window.innerWidth / imageWidth;
+    const scaleY = window.innerHeight / imageHeight;
+
+    const scale = Math.min(scaleX, scaleY);
+
+    const x = (window.innerWidth - imageWidth * scale) / 2;
+    const y = (window.innerHeight - imageHeight * scale) / 2;
+
+    return { x, y, scale };
+};
+
+
+const BackgroundMap = () => {
+    const appRef = useRef<PIXI.Application | null>(null);
+    const [scale, setScale] = useState(1);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const rendererOptions = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        // backgroundColor: 0xffffff,
+        resolution: window.devicePixelRatio || 1,
+        autoResize: true,
+    };
+
+    useEffect(() => {
+        const app = appRef.current;
+        if (!app) {
+            return
+        }
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+
+        // 更新缩放比例
+        const {x, y, scale} = calculateScale();
+        setScale(scale);
+        setPosition({x, y});
+
+        // 添加窗口调整大小的事件监听器
+        const handleResize = () => {
+            app.renderer.resize(window.innerWidth, window.innerHeight);
+
+            // 更新缩放比例
+            const {x, y, scale} = calculateScale();
+            setScale(scale);
+            setPosition({x, y});
+        };
+        window.addEventListener('resize', handleResize);
+
+        // 组件卸载时移除事件监听器
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [appRef])
 
     return (
-        <Stage>
+        <Stage
+            options={rendererOptions}
+            onMount={(app) => {
+                appRef.current = app;
+            }}
+        >
             <Sprite
-                image="https://pixijs.io/pixi-react/img/bunny.png"
-                x={400}
-                y={270}
-                anchor={{ x: 0.5, y: 0.5 }}
+                texture={PIXI.Texture.from(timeMapUrl)}
+                x={position.x}
+                y={position.y}
+                scale={new PIXI.Point(scale, scale)}
             />
-
-            <Container x={400} y={330}>
-                <Text
-                    text="Hello World"
-                    anchor={{ x: 0.5, y: 0.5 }}
-                    filters={[blurFilter]}
-                    style={
-                        new TextStyle({
-                            align: 'center',
-                            fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
-                            fontSize: 24,
-                            fill: '#ffffff',
-                        })
-                    }
-                />
-            </Container>
         </Stage>
     );
 };
 
-export default MyComponent
+export default BackgroundMap
