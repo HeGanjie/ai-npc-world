b8ba8eb6d521590ecb2e17eae241dabc4f2e2af7
Bruce He <heganjie@gmail.com>
2023-05-01 15:08:23 +0800

    refactor: 提取地图 hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import {useEffect, useRef, useState} from 'react';
-import {Sprite, Stage} from '@pixi/react';
+import { useState } from 'react';
+import { Stage } from '@pixi/react';
 import * as PIXI from 'pixi.js';
-import timeMapUrl from './assets/pixelholes-overworld-tileset-demo.png' // https://pixelhole.itch.io/pixelholes-overworld-tileset
-
-const calculateScale = () => {
-    const imageWidth = 1872 // 图片宽度;
-    const imageHeight = 1248 // 图片高度;
-
-    const scaleX = window.innerWidth / imageWidth;
-    const scaleY = window.innerHeight / imageHeight;
-
-    const scale = Math.min(scaleX, scaleY);
-
-    const x = (window.innerWidth - imageWidth * scale) / 2;
-    const y = (window.innerHeight - imageHeight * scale) / 2;
-
-    return { x, y, scale };
-};
-
+import { useMap } from "./component/map.tsx"; // https://pixelhole.itch.io/pixelholes-overworld-tileset
 
 const BackgroundMap = () => {
-    const appRef = useRef<PIXI.Application | null>(null);
-    const [scale, setScale] = useState(1);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [app, setApp] = useState<PIXI.Application | null>(null);
 
     const rendererOptions = {
         width: window.innerWidth,
@@ -32,49 +14,17 @@ const BackgroundMap = () => {
         autoResize: true,
     };
 
-    useEffect(() => {
-        const app = appRef.current;
-        if (!app) {
-            return
-        }
-        app.renderer.resize(window.innerWidth, window.innerHeight);
-
-        // 更新缩放比例
-        const {x, y, scale} = calculateScale();
-        setScale(scale);
-        setPosition({x, y});
-
-        // 添加窗口调整大小的事件监听器
-        const handleResize = () => {
-            app.renderer.resize(window.innerWidth, window.innerHeight);
-
-            // 更新缩放比例
-            const {x, y, scale} = calculateScale();
-            setScale(scale);
-            setPosition({x, y});
-        };
-        window.addEventListener('resize', handleResize);
-
-        // 组件卸载时移除事件监听器
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [appRef])
+    const mapSprite = useMap(app)
 
     return (
-        <Stage
-            options={rendererOptions}
-            onMount={(app) => {
-                appRef.current = app;
-            }}
-        >
-            <Sprite
-                texture={PIXI.Texture.from(timeMapUrl)}
-                x={position.x}
-                y={position.y}
-                scale={new PIXI.Point(scale, scale)}
-            />
-        </Stage>
+      <Stage
+        options={rendererOptions}
+        onMount={(app) => {
+            setApp(app);
+        }}
+      >
+          {mapSprite}
+      </Stage>
     );
 };
 
